2ee1a029336475943b4cbd49911ccc21ce46859b
thelethalcode <kousshikraj.raj@gmail.com>
2020-02-13 23:28:18 +0530

    Add git

---
+++ README.md
@@ -0,0 +1,23 @@
+# OpenSoft 2018 Project - DigiCon
+
+The detailed explanation of the work is given in the "Documentation.pdf"  
+
+## Dependencies
+
+Docker
+
+## Running
+
+### Ubunutu
+
+For running DigiCon, execute the shellscript `sudo -s source shellscript.sh`
+
+If working behind a proxy uncomment the required lines in the file 'shellscript.sh'
+
+### Others
+
+Install docker and docker-compose.
+
+Then run `sudo docker-compose up` from inside the directory 
+
+
---
+++ shellscript.sh
@@ -0,0 +1,12 @@
+curl -fsSL https://get.docker.com | sh
+
+## Uncomment the following lines if working behind a proxy, replacing the proper proxy address
+
+# mkdir -p /etc/systemd/system/docker.service.d
+# touch /etc/systemd/system/docker.service.d/http-proxy.conf
+# echo -e "[Service]\nEnvironment=\"HTTP_PROXY=http://172.16.2.30:8080/\"" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+# systemctl daemon-reload
+# systemctl restart docker
+# apt install -y docker-compose
+
+docker-compose up
